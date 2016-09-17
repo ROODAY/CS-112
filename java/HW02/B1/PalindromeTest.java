79de8a20eeeca0606d0a1174eca93b79ed3d6b36
Rudhra Raveendran <rudytheninja@gmail.com>
2016-09-17 14:19:46 -0400

    started hw02

---
+++ java/HW02/B1/PalindromeTest.java
@@ -0,0 +1,53 @@
+/* 
+ * File: PalindromeTest.java
+ * Author: Rudhra Raveendran (rooday@bu.edu)
+ * Date: 09/15/2016
+ * Purpose: This class tests if user input is a palindrome
+ */
+
+package HW02.B1;
+import java.util.Scanner;
+
+public class PalindromeTest { 
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to the Palindrome Test Program!");
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("\nType in a sentence or Control-d to end:"); 
+
+        while(userInput.hasNextLine()) {
+            String line = userInput.nextLine();
+            line = line.toLowerCase();
+
+            char[] charsToRemove = { '.', ',', ':', ';', '!', '?', '"', '\'', '/', '-', '(', ')', '~'};
+            for (int i = 0; i < charsToRemove.length; i++) {
+                line = line.replace(Character.toString(charsToRemove[i]), "");
+            }
+
+            String cleanedLine = "";
+
+            for (int i = 0; i < line.length(); i++) {
+                char a = line.charAt(i);
+                if (Character.isWhitespace(a) == false) {
+                    cleanedLine += Character.toString(a);
+                }
+            }
+
+            boolean isPalindrome = true;
+
+            for (int i = 0; i < cleanedLine.length(); i++) {
+                if (!(Chracter.toString(cleanedLine.charAt(i)).equals(Chracter.toString(cleanedLine.charAt(cleanedLine.length - 1 - i))))) {
+                    isPalindrome = false;
+                }
+            }
+            
+            if (isPalindrome) {
+                System.out.println("Palindrome!");
+            } else {
+                System.out.println("Not a palindrome!");
+            }
+        }
+        System.out.println("bye!");
+    }
+}
