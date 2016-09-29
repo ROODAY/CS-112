7bb9aeb20f91a73bcef3d60a1d2a9d03c3af9754
Rudhra Raveendran <rudytheninja@gmail.com>
2016-09-29 13:41:47 -0400

    started hw04 b1

---
+++ java/HW04/B1/Queueable.java
@@ -0,0 +1,12 @@
+/* File: Queueable.java
+ * Date: 9/27/16
+ * Author:  Wayne Snyder (waysnyder@gmail.com)
+ * Class: CS 112, Fall 2016
+ * Purpose: Example of Interface for Stack.java in Lab 04
+ */
+
+package HW04.B1;
+
+public interface Queueable {
+    
+}
