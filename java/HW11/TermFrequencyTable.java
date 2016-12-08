d060cf133018c98967535fac28c4fec4a9bf40e7
ROODAY <rudytheninja@gmail.com>
2016-12-07 19:40:08 -0500

    moard

--- java/HW11/TermFrequencyTable.java
+++ java/HW11/TermFrequencyTable.java
@@ -4,10 +4,11 @@
  * Date: 12/7/2016
  * Purpose: This class calculates the cosine similarity between two documents
  */
+import java.util.Iterator;
 
- public class TermFrequencyTable {
-
- 	// Standard Node subclass to store terms
+public class TermFrequencyTable implements Iterable<int[]>{
+    
+    // Standard Node subclass to store terms
     public static class Node {
         public String term;
         public int[] termFreq = new int[2];
@@ -19,21 +20,21 @@
             this.termFreq[docNum]++;
         }
     }
-
-    private final int SIZE = 101;       // Size of array (prime number close to 100)
+    
+    private final int SIZE = 101;        // Size of array (prime number close to 100)
     private Node [] T = new Node[SIZE];  // Array to hold linkedlists of articles
     private final String [] blackList = { "the", "of", "and", "a", "to", "in", "is", 
-    "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", 
-    "his", "they", "i", "at", "be", "this", "have", "from", "or", "one", 
-    "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", 
-    "your", "can", "said", "there", "use", "an", "each", "which", "she", 
-    "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", 
-    "then", "them", "these", "so", "some", "her", "would", "make", "like", 
-    "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", 
-    "number", "no", "way", "could", "people",  "my", "than", "first", "water", 
-    "been", "call", "who", "oil", "its", "now", "find", "long", "down", "day", 
-    "did", "get", "come", "made", "may", "part" }; // black list of common strings
-
+        "you", "that", "it", "he", "was", "for", "on", "are", "as", "with", 
+        "his", "they", "i", "at", "be", "this", "have", "from", "or", "one", 
+        "had", "by", "word", "but", "not", "what", "all", "were", "we", "when", 
+        "your", "can", "said", "there", "use", "an", "each", "which", "she", 
+        "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", 
+        "then", "them", "these", "so", "some", "her", "would", "make", "like", 
+        "him", "into", "time", "has", "look", "two", "more", "write", "go", "see", 
+        "number", "no", "way", "could", "people",  "my", "than", "first", "water", 
+        "been", "call", "who", "oil", "its", "now", "find", "long", "down", "day", 
+        "did", "get", "come", "made", "may", "part" }; // black list of common strings
+    
     // Simple hash function
     private int hash(String x) {
         char ch[];
@@ -45,41 +46,42 @@
             sum += ch[i];
         return sum % SIZE;
     }
-
+    
     // Initialize the hashtable from two documents
     public void initialize(String one, String two) {
-    	String[] temp1 = one.toLowerCase().split(" ");
-    	String[] temp2 = two.toLowerCase().split(" ");
-    	String[] ones = new String[temp1.length], twos = new String[temp2.length];
-
-    	int index = 0;
-    	for (int i = 0; i < temp1.length; i++) {
-    		boolean safe = true;
-    		for (int j = 0; j < blackList.length; j++) {
-    			if (temp1[i].equals(blackList[j])) {
-    				safe = false;
-    				break;
-    			}
-    			if (safe) ones[index++] = temp1[i];
-    		}
-    	}
-    	index = 0;
-    	for (int i = 0; i < temp2.length; i++) {
-    		boolean safe = true;
-    		for (int j = 0; j < blackList.length; j++) {
-    			if (temp2[i].equals(blackList[j])) {
-    				safe = false;
-    				break;
-    			}
-    			if (safe) twos[index++] = temp2[i];
-    		}
-    	}
+        String[] temp1 = one.toLowerCase().split(" ");
+        String[] temp2 = two.toLowerCase().split(" ");
+        String[] ones = new String[temp1.length];
+        String[] twos = new String[temp2.length];
+        
+        int index = 0;
+        for (int i = 0; i < temp1.length; i++) {
+            boolean safe = true;
+            for (int j = 0; j < blackList.length; j++) {
+                if (temp1[i].equals(blackList[j])) {
+                    safe = false;
+                    break;
+                }
+                if (safe) ones[index++] = temp1[i];
+            }
+        }
+        index = 0;
+        for (int i = 0; i < temp2.length; i++) {
+            boolean safe = true;
+            for (int j = 0; j < blackList.length; j++) {
+                if (temp2[i].equals(blackList[j])) {
+                    safe = false;
+                    break;
+                }
+                if (safe) twos[index++] = temp2[i];
+            }
+        }
         
         for (int i = 0; i < temp1.length; i++) {
-        	if (!temp1[i].equals("")) insert(temp1[i], 0);
+            if (!temp1[i].equals("")) insert(temp1[i], 0);
         }
         for (int i = 0; i < temp2.length; i++) {
-        	if (!temp2[i].equals("")) insert(temp2[i], 1);
+            if (!temp2[i].equals("")) insert(temp2[i], 1);
         }
     }
     
@@ -100,15 +102,95 @@
             return n;
         }
     }
-
+    
     // Return the cosine similarity of the terms for the two documents stored in this table
-    double cosineSimilarity() {
-
+    public double cosineSimilarity() {
+        Iterator<int[]> it = iterator();
+        int[] A =  new int[SIZE];
+        int[] B = new int[SIZE];
+        int index = 0;
+        while (it.hasNext()) {
+            int[] n = it.next();
+            A[index] = n[0];
+            B[index++] = n[1];
+        }
+        return dotProduct(A,B) / (Math.sqrt(dotProduct(A,A)) * Math.sqrt(dotProduct(B,B)));
     }
-
+    
+    private double dotProduct(int[] A, int[] B) {
+        double sum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i] * B[i];
+        }
+        return sum;
+    }
+    
+    public Iterator<int[]> iterator() {
+        return new It();
+    }
+    
+    private class It implements Iterator<int[]> {
+        private int index = 0;
+        private Node cursor;
+        
+        public It() { 
+            setCursors();                       
+        }
+        
+        // Finds next non-null index in array of string linkedlists
+        private void setCursors() {
+            if (T[index] == null) {
+                if (++index < SIZE) {
+                    setCursors();
+                }
+            } else {
+                cursor = T[index];
+            }
+        }
+        
+        public boolean hasNext() {
+            return index < SIZE && cursor != null;
+        }
+        
+        private boolean hasNextCursor() {
+            return cursor != null;
+        }
+        
+        // Returns next string Node, calls setCursors when it reaches the end of a linkedlist
+        public int[] next() {
+            if(hasNext()) {
+                Node result = cursor;
+                cursor = cursor.next;
+                if (cursor == null) {
+                    index++;
+                    setCursors();
+                }
+                return result.termFreq;
+            }
+            return null;
+        } 
+        
+        public void remove() {
+        }
+    }
+    
     public static void main(String[] args) {
-    	ArticleTable L = new ArticleTable(); 
-    	String one = "", two = "";
-	    L.initialize(one, two);
-   	}
+        TermFrequencyTable L = new TermFrequencyTable(); 
+        String one = "A B";
+        String two = "A A B B";
+        L.initialize(one, two);
+        System.out.println("One: " + one + ", Two: " + two + ", Similarity: " + L.cosineSimilarity());
+        
+        L = new TermFrequencyTable(); 
+        one = "A B";
+        two = "C D";
+        L.initialize(one, two);
+        System.out.println("One: " + one + ", Two: " + two + ", Similarity: " + L.cosineSimilarity());
+        
+        L = new TermFrequencyTable(); 
+        one = "CS112 HW11";
+        two = "CS112 HW11 HW11";
+        L.initialize(one, two);
+        System.out.println("One: " + one + ", Two: " + two + ", Similarity: " + L.cosineSimilarity());
+    }
 }
